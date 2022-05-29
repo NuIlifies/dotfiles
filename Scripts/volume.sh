5e999a493de62fb83876c5e954104030c5645e48
Nicholas <65469437+NuIlifies@users.noreply.github.com>
2022-05-29 09:37:39 -0700

    Update volume.sh

--- Scripts/volume.sh
+++ Scripts/volume.sh
@@ -44,5 +44,4 @@ case $1 in
     ;;
   down)
 
-^G Help         ^O Write Out    ^W Where Is     ^K Cut          ^T Execute      ^C Location
-^X Exit         ^R Read File    ^\ Replace      ^U Paste        ^J Justify      ^/ Go To Line
+
